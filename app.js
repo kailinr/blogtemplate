3040822a11348008ee83495e18ce69f7681b705d
kailin <kailin@torvadata.com>
2021-02-18 23:10:57 -0800

    EJS tempalates added to home.ejs, and node modules added to gitignore

--- app.js
+++ app.js
@@ -16,8 +16,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 
+app.get('/', function (req, res){
 
-
+  res.render('home.ejs', {startingContent:homeStartingContent});
+});
 
 
 
